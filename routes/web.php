208be2503c16aa1f73bf49435ebe2ef5b024820b
adilmaqsood786 <adilmaqsoodahmad@gmail.com>
2026-08-01 11:11:56 +0500

    changes

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,19 @@ use App\Http\Controllers\OrderController;
 
 
 
+use Illuminate\Support\Facades\Storage;
 
+Route::get('/storage/{path}', function ($path) {
+    $fullPath = storage_path('app/public/' . $path);
+
+    if (!file_exists($fullPath)) {
+        abort(404);
+    }
+
+    return response()->file($fullPath, [
+        'Access-Control-Allow-Origin' => '*',
+    ]);
+})->where('path', '.*');
 
 
 
