b17f98223b3ffa8014ca6c919375a46cadde6b34
adilmaqsood786 <adilmaqsoodahmad@gmail.com>
2026-09-03 00:39:56 +0500

    fix

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -293,8 +293,8 @@ class UserController extends Controller
                 'address' => $request->address,
                 'vendor_city' => $request->vendor_city,
                 'vendor_country' => $request->vendor_country,
-                'commission_rate' => $request->commission_rate ?? 10,
-                'is_active' => $request->is_active ? 1 : 0,
+                'commission_rate' =>  20.0,
+                'is_active' => 1,
             ];
 
             $forUpdate && $user->vendor
